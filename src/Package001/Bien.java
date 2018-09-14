09a03e8e42b99230cf3d1ffeb3a04f4f3fdc9c48
namnh89 <mr.allblue@gmail.com>
2018-09-14 10:35:49 +0700

    initial commit

---
+++ src/Package001/Bien.java
@@ -0,0 +1,17 @@
+package Package001;
+
+public class Bien {
+	
+	public void xinChao(){
+		int n=100;//Bien local
+		System.out.println("value of n is: "+n);
+	}
+
+	public static void main(String[] args) {
+		Bien chao = new Bien();
+		chao.xinChao();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Package001/HelloWorld.java
@@ -0,0 +1,9 @@
+package Package001;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Chao moi nguoi.");
+	}
+
+}
